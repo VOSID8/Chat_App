98fe75c15416cca7725b73c02d333746f6d5b8b5
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-10-30 13:46:21 +0530

    public updated

---
+++ public/javascripts/chat.js
@@ -0,0 +1,18 @@
+const inputField = document.getElementById("chat-input");
+const outputArea = document.getElementById("chat-area");
+
+const socketRoute = document.getElementById("ws-route").value;
+const socket = new WebSocket(socketRoute.replace("http","ws"));
+
+inputField.onkeydown = (event) => {
+  if(event.key === 'Enter') {
+    socket.send(inputField.value + "wow");
+    inputField.value = '';
+  }
+}
+
+socket.onopen = (event) => socket.send("New user connected.");
+
+socket.onmessage = (event) => {
+  outputArea.value += '\n' + event.data;
+}
